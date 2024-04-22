faccf5df8d6da84adf1e62e7b8e440259abc8a30
nkdev2024 <167497378+nkdev2024@users.noreply.github.com>
2024-04-22 08:55:08 +0000

    make a component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [RouterOutlet,FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule,MatCardModule],
 })
 export class AppComponent {
   title = 'my-angular-project';
