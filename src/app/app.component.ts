3786219f6849a6d8b2cf269fcabf60d1d43d349d
nkdev2024 <167497378+nkdev2024@users.noreply.github.com>
2024-05-02 07:06:57 +0000

    add file upload api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,11 +26,42 @@ import { Observable } from 'rxjs';
 })
 export class AppComponent {
   title = 'my-angular-project';
-  backendUrl = 'https://psychic-orbit-5gvjxx5gj9jrfq99-8080.app.github.dev';
+  // backendUrl = 'https://psychic-orbit-5gvjxx5gj9jrfq99-8080.app.github.dev';
+  backendUrl = 'http://44.193.168.210:8080';
   dataList: any = [];
 
   constructor(private http: HttpClient) {}
+ 
+  // file upload api
+  fileUpload(): void {
+       // Capture the file input element
+       const fileInput: any = document.querySelector('input[type="file"]') as HTMLInputElement;
+       const file = fileInput.files[0];
 
+       // Create FormData object and append form data
+        const formData = new FormData();
+   
+      //  formData.append('password', password);
+        formData.append('file', file);
+
+        
+    // Send data to the backend using HttpClient
+    this.http.post(`${this.backendUrl}/fileUpload`, formData)
+    .subscribe(
+      response => {
+        console.log('Image uploaded successfull.');
+        // Handle success response from the server
+      },
+      error => {
+        // try catch error handler method
+        console.error('Image is not uploaded successfully.');
+        // Handle error response from the server
+      }
+    );
+
+  }
+
+  // creating/sending user data api
   sendData(): void {
     // Get input values
     const userId = (document.querySelector('input[placeholder="userId"]') as HTMLInputElement).value;
@@ -38,6 +69,7 @@ export class AppComponent {
     const lastName = (document.querySelector('input[placeholder="VXYZ"]') as HTMLInputElement).value;
     const rollNumber = (document.querySelector('input[placeholder="111"]') as HTMLInputElement).value;
     const password = (document.querySelector('input[placeholder="password"]') as HTMLInputElement).value;
+
 
     // Create an object with the input data
     const data = {
@@ -56,12 +88,14 @@ export class AppComponent {
           // Handle success response from the server
         },
         error => {
+          // using promise for async await method
           console.error('Error sending data:', error);
           // Handle error response from the server
         }
       );
   }
 
+  // fetch user data request
   fetchData(): void{
       // Send data to the backend using HttpClient
     this.http.get(`${this.backendUrl}/getAllusers`)
@@ -71,11 +105,13 @@ export class AppComponent {
         this.dataList = data;
       },
       (error) => {
+        // async await method way using promises
         console.error('Error fetching data:', error);
       }
     );
   }
 
+  // update user info/data api 
   updateItem(datas: any, i: any): void{
     console.log(i)
     this.http.put(`${this.backendUrl}/updateUser`, datas)
@@ -85,11 +121,13 @@ export class AppComponent {
         this.dataList[i] = data;
       },
       (error) => {
+        // using async await method using promises
         console.error('Error fetching data:', error);
       }
     );
   }
 
+  // delete user data api
   deleteItem(datas: any , i: any): void {
   console.log(datas + i);
   this.http.delete(`${this.backendUrl}/deleteUser?rollNumber=${datas.rollNumber}`)
@@ -101,6 +139,7 @@ export class AppComponent {
         console.log(this.dataList);
       },
       error: (error) => {
+        // using async await using promises method
         console.error('Error fetching data:', error);
       }
     });
